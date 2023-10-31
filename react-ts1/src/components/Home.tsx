36b5a621cb4fc338d43d60a2f2ceba10c580c654
arthyun <heun3316@naver.com>
2023-10-31 22:54:19 +0900

    commit9

--- react-ts1/src/components/Home.tsx
+++ react-ts1/src/components/Home.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Home = () => {
-   return <div>Home</div>;
+   const openKey = "1fa67d81d0db4d32bbd263f5a3cc05af";
+
+   useEffect(() => {
+      fetch(`https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt?KEY=${openKey}&Type=json&pIndex=1&pSize=200&SIGUN_NM=과천시`)
+         .then((res) => res.json())
+         .then((data) => console.log(data));
+   }, []);
+
+   return <div>경기도 맛집 찾기</div>;
 };
 
 export default Home;
