ccdf7f1a2e5e4eb81a35858f932ebfd7270460af
arthyun <heun3316@naver.com>
2023-11-01 20:10:00 +0900

    commit10

--- react-ts1/src/components/Home.tsx
+++ react-ts1/src/components/Home.tsx
@@ -1,15 +1,70 @@
-import React, { useEffect } from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import styled from "styled-components";
+
+// Custom Components
+const HomeContainer = styled.div`
+   width: 100%;
+   padding: 1rem;
+   box-sizing: border-box;
+   border: 1px solid #fff;
+`;
+const LabelSearch = styled.label`
+   display: none;
+   font-size: 20px;
+   margin-right: 15px;
+`;
+const InputSearch = styled.input`
+   width: 300px;
+   padding: 10px 5px;
+   font-size: 16px;
+`;
 
 const Home = () => {
-   const openKey = "1fa67d81d0db4d32bbd263f5a3cc05af";
+   const [text, setText] = useState<string>("");
+   const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
+      const { value } = e.target;
+      setText(value);
+   };
+
+   // 초기 데이터 호출
+   const openKey: string = "1fa67d81d0db4d32bbd263f5a3cc05af";
+   const getData = async (text: string): Promise<void> => {
+      try {
+         const response = await fetch(`https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt?KEY=${openKey}&Type=json&pIndex=1&pSize=200&SIGUN_NM=${text}`);
+         const result = await response.json();
+         console.log(result);
+         return result;
+      } catch (err) {
+         console.error(err);
+      }
+   };
 
    useEffect(() => {
-      fetch(`https://openapi.gg.go.kr/PlaceThatDoATasteyFoodSt?KEY=${openKey}&Type=json&pIndex=1&pSize=200&SIGUN_NM=과천시`)
-         .then((res) => res.json())
-         .then((data) => console.log(data));
+      getData(text);
    }, []);
 
-   return <div>경기도 맛집 찾기</div>;
+   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      // console.log("form!");
+      getData(text);
+   };
+
+   return (
+      <HomeContainer>
+         <h3>경기도 맛집 찾기</h3>
+         <form onSubmit={onSubmit}>
+            <LabelSearch htmlFor="search">검색창</LabelSearch>
+            <InputSearch
+               id="search"
+               type="text"
+               value={text}
+               onChange={onChangeText}
+               placeholder="검색어를 입력하세요"
+            />
+            <button type="submit">검색</button>
+         </form>
+      </HomeContainer>
+   );
 };
 
 export default Home;
