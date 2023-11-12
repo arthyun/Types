fab594edfd7d1f59d57bcf903cc04d64c83efd37
arthyun <heun3316@naver.com>
2023-11-12 19:57:13 +0900

    commit18

--- react-ts1/src/components/TailwindComponent.tsx
+++ react-ts1/src/components/TailwindComponent.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const TailwindComponent = () => {
+   const listRef = useRef<HTMLLIElement[]>([]);
+
+   useEffect(() => {
+      if (listRef.current) {
+         listRef.current[0].classList.add("text-3xl");
+         listRef.current[1].classList.add("text-3xl");
+         listRef.current[2].classList.add("text-3xl");
+      }
+   }, []);
+
    return (
       <div className="md:container md:mx-auto">
-         <ul className="flex gap-10">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+         <ul className="flex gap-10 justify-center">
+            <li
+               ref={(el) => (listRef.current[0] = el as HTMLLIElement)}
+               className="p-20 bg-white font-bold text-red-500 rounded-lg w-16 md:w-32 lg:w-48"
+            >
+               1
+            </li>
+            <li
+               ref={(el) => (listRef.current[1] = el as HTMLLIElement)}
+               className="p-20 bg-white font-bold text-red-500 rounded-lg w-16 md:w-32 lg:w-48"
+            >
+               2
+            </li>
+            <li
+               ref={(el) => (listRef.current[2] = el as HTMLLIElement)}
+               className="p-20 bg-white font-bold text-red-500 rounded-lg w-16 md:w-32 lg:w-48"
+            >
+               3
+            </li>
          </ul>
       </div>
    );
