04876d6ef2e40b02a249c1fc5bdb3a572a082b6c
arthyun <heun3316@naver.com>
2023-11-13 19:45:21 +0900

    commit19

--- react-ts1/src/components/TailwindComponent.tsx
+++ react-ts1/src/components/TailwindComponent.tsx
@@ -12,25 +12,25 @@ const TailwindComponent = () => {
    }, []);
 
    return (
-      <div className="md:container md:mx-auto">
-         <ul className="flex gap-10 justify-center">
+      <div className="md:w-full md:container md:mx-auto">
+         <ul className="flex md:flex-col lg:flex-row gap-10 justify-center w-full">
             <li
                ref={(el) => (listRef.current[0] = el as HTMLLIElement)}
-               className="p-20 bg-white font-bold text-red-500 rounded-lg w-16 md:w-32 lg:w-48"
+               className="bg-white font-bold text-red-500 rounded-lg hover:bg-pink-500 md:w-40 lg:w-60 py-16"
             >
                1
             </li>
             <li
                ref={(el) => (listRef.current[1] = el as HTMLLIElement)}
-               className="p-20 bg-white font-bold text-red-500 rounded-lg w-16 md:w-32 lg:w-48"
+               className="bg-white font-bold text-red-500 rounded-lg md:w-40 lg:w-60 py-16"
             >
                2
             </li>
             <li
                ref={(el) => (listRef.current[2] = el as HTMLLIElement)}
-               className="p-20 bg-white font-bold text-red-500 rounded-lg w-16 md:w-32 lg:w-48"
+               className="bg-white font-bold text-red-500 rounded-lg md:w-40 lg:w-60 py-16 relative"
             >
-               3
+               <span className="w-16 h-6 overflow-hidden text-sm bg-gray-500 absolute top-0 right-0">position</span>
             </li>
          </ul>
       </div>
