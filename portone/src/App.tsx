0d1927a323ee603e643ca323f9e15cb205379bc1
heun3316 <heun3316@naver.com>
2024-01-13 19:39:49 +0900

    commit36

--- portone/src/App.tsx
+++ portone/src/App.tsx
@@ -3,22 +3,33 @@ import { useEffect } from 'react';
 import reactLogo from './assets/images/react.svg';
 import viteLogo from './assets/images/vite.svg';
 import './assets/styles/App.css';
-import * as PortOne from '@portone/browser-sdk/v2';
 
-function App() {
-  // 결제 연동
-  const requestPay = () => {
-    PortOne.requestPayment({
-      storeId: 'store-4ff4af41-85e3-4559-8eb8-0d08a2c6ceec',
-      paymentId: `payment-${crypto.randomUUID()}`,
-      orderName: '나이키 와플 트레이너 2 SD',
-      totalAmount: 1000,
-      currency: 'CURRENCY_KRW',
-      pgProvider: 'PG_PROVIDER_TOSSPAYMENTS',
-      payMethod: 'CARD'
-    });
-  };
+interface TestTypes {
+  pg: string;
+  pay_method: string;
+  merchant_uid: string;
+  amount: number;
+  name: string;
+  buyer_name: string;
+  buyer_tel: string;
+  buyer_email: string;
+  buyer_addr: string;
+  buyer_postcode: string;
+}
+interface CallbackParams {
+  error_msg: string;
+  imp_uid?: string;
+  merchant_uid?: string;
+  pay_method?: string;
+  pg_provider?: string;
+  pg_type?: string;
+  success: boolean;
+}
+interface AppTypes {
+  callback: (response: CallbackParams) => void;
+}
 
+const App = () => {
   // Cookies
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
@@ -47,6 +58,65 @@ function App() {
     getToken();
   }, []);
 
+  // 결제관련 (jQuery 있어야함)
+  const confirmPayment = async () => {
+    // const response = await fetch(`/api/payments?imp_uid[]=${import.meta.env.VITE_APP_STORE_CODE}`, {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Authorization: `Bearer ${cookies.accessToken}`
+    //   }
+    // });
+    const response = await fetch(`/api/payments/${import.meta.env.VITE_APP_STORE_CODE}/balance`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${cookies.accessToken}`
+      }
+    });
+    const result = await response.json();
+    if (result.code === 0) {
+      alert('조회 성공');
+      return result;
+    } else {
+      alert(result.message);
+    }
+  };
+
+  const onClickPayment = () => {
+    /* 가맹점 식별하기 */
+    // const { IMP } = window;
+    window.IMP.init(import.meta.env.VITE_APP_STORE_CODE);
+
+    /* 2. 결제 데이터 정의하기 */
+    const testData: TestTypes = {
+      pg: 'html5_inicis', // PG사
+      pay_method: 'card', // 결제수단
+      merchant_uid: `test_${new Date().getTime()}`, // 주문번호
+      name: '테스트 결제', // 주문명
+      amount: 1, // 결제금액
+      buyer_name: '손현호', // 구매자 이름
+      buyer_tel: '010-0000-0000', // 구매자 전화번호
+      buyer_email: 'heun3316@naver.com', // 구매자 이메일
+      buyer_addr: '신사동 661-16', // 구매자 주소
+      buyer_postcode: '06018' // 구매자 우편번호
+      // popup: false
+    };
+
+    /* 결제 창 호출하기 */
+    window.IMP.request_pay(testData, callback);
+  };
+
+  /* 3. 콜백 함수 정의하기 */
+  const callback: AppTypes['callback'] = (response) => {
+    const { success, error_msg } = response;
+    if (success) {
+      console.log(response);
+      alert('결제 성공');
+    } else {
+      alert(`결제 실패: ${error_msg}`);
+    }
+  };
+
   return (
     <>
       <div>
@@ -59,7 +129,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={requestPay}>결제하기</button>
+        <button type="button" onClick={confirmPayment}>
+          결제조회
+        </button>
+        <button type="button" onClick={onClickPayment}>
+          결제하기
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -67,6 +142,6 @@ function App() {
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
-}
+};
 
 export default App;
