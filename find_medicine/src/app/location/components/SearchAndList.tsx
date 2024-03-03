e1f3d92f739ce48d0726508b9fdbf581d12f7ea6
arthyun <heun3316@naver.com>
2024-03-03 16:29:05 +0900

    build test v2

--- find_medicine/src/app/location/components/SearchAndList.tsx
+++ find_medicine/src/app/location/components/SearchAndList.tsx
@@ -70,7 +70,7 @@ const SearchAndList = ({ data, pagiData }: { data: any; pagiData: any }) => {
 
   //  화면 진입시
   useEffect(() => {
-    getSido().then((res) => setSidoInfo(res.response.result.featureCollection.features.reduce((acc: any, cur: any) => acc.concat(cur.properties), [])));
+    getSido().then((res) => setSidoInfo(res.response.result?.featureCollection?.features?.reduce((acc: any, cur: any) => acc.concat(cur.properties), [])));
     if (!ssr) {
       getData(selectForm1, selectForm2, text, limit, pageCnt).then((result) => {
         setRowList(result.response?.body?.items?.item);
@@ -186,7 +186,7 @@ const SearchAndList = ({ data, pagiData }: { data: any; pagiData: any }) => {
                 );
               })}
         </ul>
-        {rowList?.length === 0 && <ResultNoData />}
+        {!ssr && rowList?.length === 0 && <ResultNoData />}
       </div>
       <ResultListPaging limit={limit} page={pageCnt || pagiData.pageNo} totalpage={totalpage} totalcnt={totalcnt || pagiData.totalCount} setPageCnt={setPageCnt} setSsr={setSsr} />
     </div>
