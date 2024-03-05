0d8690ddb17c8740302aada6128ae3d09221aeac
heun3316 <heun3316@naver.com>
2024-03-05 10:18:51 +0900

    double quote -> single quote

--- next14/app/about-us/page.tsx
+++ next14/app/about-us/page.tsx
@@ -1,13 +1,13 @@
-import { Metadata } from "next";
-import React from "react";
+import { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
-  title: "AboutUs"
+  title: 'AboutUs'
 };
 
 export default function AboutUs() {
   return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
       <h3>About Us!</h3>
     </div>
   );
