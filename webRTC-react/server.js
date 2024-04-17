996db720bfddb408483d536149d35414e70dfd9d
arthyun <heun3316@naver.com>
2024-04-17 17:29:55 +0900

    modify node.js at webRTC-react

--- webRTC-react/server.js
+++ webRTC-react/server.js
@@ -1,81 +1,51 @@
-let users = {};
-let socketToRoom = {};
-const maximum = 2;
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use('/', express.static('public'));
 
 io.on('connection', (socket) => {
-  // 1:1 에서는 이런 식으로 구현하지 않아도 되지만 글쓴이는 1:N을 먼저 구현해서 이 형태로 남겨뒀습니다.
-  // email 부분은 무시하셔도 무방합니다.
-  socket.on('join_room', (data) => {
-    // user[room]에는 room에 있는 사용자들이 배열 형태로 저장된다.
-    // room이 존재한다면
-    if (users[data.room]) {
-      const length = users[data.room].length;
-      // 최대 인원을 충족시켰으면 더 이상 접속 불가
-      if (length === maximum) {
-        socket.to(socket.id).emit('room_full');
-        return;
-      }
-      // 인원이 최대 인원보다 적으면 접속 가능
-      users[data.room].push({ id: socket.id, email: data.email });
+  socket.on('join', (roomId) => {
+    const roomClients = io.sockets.adapter.rooms[roomId] || { length: 0 };
+    const numberOfClients = roomClients.length;
+
+    // These events are emitted only to the sender socket.
+    if (numberOfClients == 0) {
+      console.log(`Creating room ${roomId} and emitting room_created socket event`);
+      socket.join(roomId);
+      socket.emit('room_created', roomId);
+    } else if (numberOfClients == 1) {
+      console.log(`Joining room ${roomId} and emitting room_joined socket event`);
+      socket.join(roomId);
+      socket.emit('room_joined', roomId);
     } else {
-      // room이 존재하지 않는다면 새로 생성
-      users[data.room] = [{ id: socket.id, email: data.email }];
+      console.log(`Can't join room ${roomId}, emitting full_room socket event`);
+      socket.emit('full_room', roomId);
     }
-    // 해당 소켓이 어느 room에 속해있는 지 알기 위해 저장
-    socketToRoom[socket.id] = data.room;
-
-    socket.join(data.room);
-    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
-
-    // 본인을 제외한 같은 room의 user array
-    const usersInThisRoom = users[data.room].filter((user) => user.id !== socket.id);
-
-    console.log(usersInThisRoom);
-
-    // 본인에게 해당 user array를 전송
-    // 새로 접속하는 user가 이미 방에 있는 user들에게 offer(signal)를 보내기 위해
-    io.sockets.to(socket.id).emit('all_users', usersInThisRoom);
   });
 
-  // 다른 user들에게 offer를 보냄 (자신의 RTCSessionDescription)
-  socket.on('offer', (sdp) => {
-    console.log('offer: ' + socket.id);
-    // room에는 두 명 밖에 없으므로 broadcast 사용해서 전달
-    // 여러 명 있는 처리는 다음 포스트 1:N에서...
-    socket.broadcast.emit('getOffer', sdp);
+  // These events are emitted to all the sockets connected to the same room except the sender.
+  socket.on('start_call', (roomId) => {
+    console.log(`Broadcasting start_call event to peers in room ${roomId}`);
+    socket.broadcast.to(roomId).emit('start_call');
   });
+  socket.on('webrtc_offer', (event) => {
+    console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`);
+    socket.broadcast.to(event.roomId).emit('webrtc_offer', event.sdp);
+  });
+  socket.on('webrtc_answer', (event) => {
+    console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`);
+    socket.broadcast.to(event.roomId).emit('webrtc_answer', event.sdp);
+  });
+  socket.on('webrtc_ice_candidate', (event) => {
+    console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`);
+    socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event);
+  });
+});
 
-  // offer를 보낸 user에게 answer을 보냄 (자신의 RTCSessionDescription)
-  socket.on('answer', (sdp) => {
-    console.log('answer: ' + socket.id);
-    // room에는 두 명 밖에 없으므로 broadcast 사용해서 전달
-    // 여러 명 있는 처리는 다음 포스트 1:N에서...
-    socket.broadcast.emit('getAnswer', sdp);
-  });
-
-  // 자신의 ICECandidate 정보를 signal(offer 또는 answer)을 주고 받은 상대에게 전달
-  socket.on('candidate', (candidate) => {
-    console.log('candidate: ' + socket.id);
-    // room에는 두 명 밖에 없으므로 broadcast 사용해서 전달
-    // 여러 명 있는 처리는 다음 포스트 1:N에서...
-    socket.broadcast.emit('getCandidate', candidate);
-  });
-
-  // user가 연결이 끊겼을 때 처리
-  socket.on('disconnect', () => {
-    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
-    // disconnect한 user가 포함된 roomID
-    const roomID = socketToRoom[socket.id];
-    // room에 포함된 유저
-    let room = users[roomID];
-    // room이 존재한다면(user들이 포함된)
-    if (room) {
-      // disconnect user를 제외
-      room = room.filter((user) => user.id !== socket.id);
-      users[roomID] = room;
-    }
-    // 어떤 user가 나갔는 지 room의 다른 user들에게 통보
-    socket.broadcast.to(room).emit('user_exit', { id: socket.id });
-    console.log(users);
-  });
+// START THE SERVER =================================================================
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
