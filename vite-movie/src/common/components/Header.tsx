afd130ef3ecb7a713f5493545e70e4c91cec2a6b
arthyun <heun3316@naver.com>
2024-09-12 18:50:47 +0900

    헤더 구조 툴팁 형식으로 변경

--- vite-movie/src/common/components/Header.tsx
+++ vite-movie/src/common/components/Header.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { userStore } from '@/contexts/userStore';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
   // Another
@@ -11,15 +12,38 @@ const Header = () => {
   const setInfo = userStore(
     (state: { setInfo: (by: boolean) => void }) => state.setInfo
   );
+  const [isOpenTooltip, setIsOpenTooltip] = useState<boolean>(false);
 
-  const handleReturn = () => {
+  const handleMenuBtn = (e: any) => {
+    e.stopPropagation();
+    setIsOpenTooltip(!isOpenTooltip);
+  };
+
+  const handleGoHome = () => {
     setInfo(false); // 전역 처리 후 헤더 및 푸터 안보이게
     navigate('/', { replace: false });
   };
 
+  useEffect(() => {
+    document.addEventListener('click', () => setIsOpenTooltip(false));
+    // return () => document.removeEventListener('click');
+  }, []);
+
   return (
-    <header className={classes.header} onClick={handleReturn}>
-      <FontAwesomeIcon icon={faHouse} />
+    <header
+      className={classes.header}
+      onMouseEnter={() => setIsOpenTooltip(true)}
+      onMouseLeave={() => setIsOpenTooltip(false)}
+    >
+      <FontAwesomeIcon icon={faHouse} onClick={handleMenuBtn} />
+
+      {isOpenTooltip && (
+        <div className={classes.tooltop}>
+          <span onClick={handleGoHome}>Home</span>
+          <span>Menu02</span>
+          <span>Menu03</span>
+        </div>
+      )}
     </header>
   );
 };
